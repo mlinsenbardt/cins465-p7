bc9c67434d1bbc8da8fe80467d474b34183a1565
mlinsenbardt <mlinsenbardt@mail.csuchico.edu>
2013-11-09 12:49:51 -0800

    basic build

---
+++ PhotoSite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoSite::Application.config.secret_key_base = '4d99807f5b02740e70394353d695e7b4e580e8098eed3d02251bc6fb2a128464e2394202391220c6ee14798ee09c80b752375b5ef5df2e498c0f9c68029b0878'
